5fa81a63c4e9596749959de490e1b3d8e78fbfa4
chmodder <hotmailgateway@gmail.com>
2013-10-02 01:37:38 +0200

    Added security
    Only registrered users can now see "brugere.aspx"

--- brugere.aspx.cs
+++ brugere.aspx.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
 
 public partial class brugere : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+    // Tjek om brugeren er logget ind
+        // Hvis brugeren ikke er logget ind videresendes til default.aspx
+        if (Session["bruger_id"] == null)
+        {
+            Response.Redirect("default.aspx");
+        }
 
     }
 }
