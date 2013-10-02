35c3caf615542740671d302d96f13182cbe97b5f
chmodder <hotmailgateway@gmail.com>
2013-10-02 11:32:21 +0200

    Created "opretbruger.aspx"
    working

---
+++ opretbruger.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="opretbruger.aspx.cs" Inherits="opretbruger" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+    <h1>Opret ny bruger</h1>
+
+    <div class="row">
+        <div class="col-sm-6 col-md-4 col-lg-4">
+            <%-- Besked --%>
+            <%-- Div-boks med succes-meddelelse. Ikke synlig --%>
+            <asp:Panel ID="PanelMsgSuccess" runat="server" CssClass="alert alert-success alert-dismissable" Visible="False">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <strong>Brugeren</strong> er blevet oprettet
+            </asp:Panel>
+            <%-- Div-boks med fejlmeddelelse. Ikke synlig --%>
+            <asp:Panel ID="PanelMsgFejl" runat="server" CssClass="alert alert-danger alert-dismissable" Visible="False">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <strong>Bruger</strong> blev ikke oprettet på grund af fejl i filhåndtering.
+            </asp:Panel>
+
+            <%-- Inputfelter --%>
+            <div class="form-group">
+                <label>Brugernavn</label>
+                <asp:TextBox ID="TextBoxNavn" runat="server" CssClass="form-control" Placeholder="Brugernavn"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidatorNavn" runat="server" ErrorMessage="skal udfyldes" ValidationGroup="form" ControlToValidate="TextBoxNavn" Display="Dynamic"></asp:RequiredFieldValidator>
+            </div>
+            <div class="form-group">
+                <label>Email</label>
+                <asp:TextBox ID="TextBoxEmail" runat="server" CssClass="form-control" Placeholder="Email"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidatorEmail" runat="server" ErrorMessage="skal udfyldes" ValidationGroup="form" ControlToValidate="TextBoxEmail"></asp:RequiredFieldValidator>
+                <asp:RegularExpressionValidator ID="RegularExpressionValidatorEmail" runat="server" ErrorMessage="er ikke en email adresse" ValidationGroup="form" ValidationExpression="\b[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\b" ControlToValidate="TextBoxEmail" Display="Dynamic"></asp:RegularExpressionValidator>
+            </div>
+            <div class="form-group">
+                <label>Telefon nummer</label>
+                <asp:TextBox ID="TextBoxTelefon" runat="server" CssClass="form-control" Placeholder="Telefon nummer"></asp:TextBox>
+                <asp:RegularExpressionValidator ID="RegularExpressionValidatorTelefon" runat="server" ErrorMessage="er ikke et telefonnummer" ValidationGroup="form" ValidationExpression="\d{8}" ControlToValidate="TextBoxTelefon" Display="Dynamic"></asp:RegularExpressionValidator>
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <asp:TextBox ID="TextBoxPassword" runat="server" CssClass="form-control" Placeholder="Password"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidatorPassword" runat="server" ErrorMessage="skal udfyldes" ValidationGroup="form" ControlToValidate="TextBoxPassword" Display="Dynamic"></asp:RequiredFieldValidator>
+            </div>
+            <div class="form-group">
+                <label>Bruger billede</label>
+                <asp:FileUpload ID="FileUploadAvatar" runat="server" />
+            </div>
+            <asp:Button ID="ButtonOpretBruger" runat="server" Text="Opret bruger" CssClass="btn btn-success" OnClick="ButtonOpretBruger_Click" ValidationGroup="form" />
+        </div>
+    </div>
+
+</asp:Content>
+
---
+++ opretbruger.aspx.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+public partial class opretbruger : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void ButtonOpretBruger_Click(object sender, EventArgs e)
+    {
+        //try
+        //{
+            // Upload originalen til mappen /Images/Upload/Original/
+            FileUploadAvatar.SaveAs(Server.MapPath("~/Images/Upload/Original/") + FileUploadAvatar.FileName);
+
+            // Kald Metoden MakeThumbs, som laver en Reisze af originalen til 768px i bredden og uploader den til Resized_original mappen 
+            MakeThumb(FileUploadAvatar.FileName, "/Images/Upload/Original/", 768, "/Images/Upload/Resized_Original/");
+
+            // Kald Metoden MakeThumbs, som laver en Thumbnail og uploader den til Thumbs mappen
+            MakeThumb(FileUploadAvatar.FileName, "/Images/Upload/Original/", 120, "/Images/Upload/Thumbs/");
+
+            // opret forbindelse til databasen
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString());
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            // SQL strengen
+            cmd.CommandText = "INSERT INTO brugere (brugernavn, email, telefon, password, avatar) VALUES (@brugernavn, @email, @telefon, @password, @avatar)";
+
+            // Tilføj parametrer (input fra brugeren / TextBox fra .aspx siden) til din SQL streng
+            cmd.Parameters.Add("@brugernavn", SqlDbType.NVarChar).Value = TextBoxNavn.Text;
+            cmd.Parameters.Add("@email", SqlDbType.NVarChar).Value = TextBoxEmail.Text;
+            cmd.Parameters.Add("@telefon", SqlDbType.Int).Value = TextBoxTelefon.Text;
+            cmd.Parameters.Add("@password", SqlDbType.NVarChar).Value = TextBoxPassword.Text;
+            cmd.Parameters.Add("@avatar", SqlDbType.NVarChar).Value = FileUploadAvatar.FileName;
+
+            // Åben for forbindelsen til databasen
+            conn.Open();
+
+            // Udfør SQL komandoen
+            cmd.ExecuteNonQuery();
+
+            // Luk for forbindelsen til databasen
+            conn.Close();
+
+            // Besked til brugeren om at brugeren er oprettet
+            PanelMsgSuccess.Visible = true;
+
+            // reset form
+            TextBoxNavn.Text = "";
+            TextBoxEmail.Text = "";
+            TextBoxTelefon.Text = "";
+            TextBoxPassword.Text = "";
+
+            Page_Load(null, null);
+        //}
+
+        //catch (Exception)
+        //{
+        //    PanelMsgFejl.Visible = true;
+        //}
+    }
+
+
+    #region simpel upload til mappe og database
+
+    #endregion
+
+    #region Metoden MakeThumb
+    /// <summary>
+    /// Opret et Thumb, baseret på en fil og en mappe
+    /// </summary>
+    /// <param name="Filename">Hvad hedder filen</param>
+    /// <param name="UploadFolder">Hvor er den uploadet til</param>
+    private void MakeThumb(string Filename, string UploadFolder, int bredde, string thumbuploadfolder)
+    {
+        // find det uploadede image
+        System.Drawing.Image OriginalImg = System.Drawing.Image.FromFile(Server.MapPath("~/") + UploadFolder + Filename);
+
+        // find højde og bredde på image
+        int originalWidth = OriginalImg.Width;
+        int originalHeight = OriginalImg.Height;
+
+        // bestem den nye bredde på det thumbnail som skal laves
+        int newWidth = bredde;
+
+        // beregn den nye højde på thumbnailbilledet
+        double ratio = newWidth / (double)originalWidth;
+        int newHeight = Convert.ToInt32(ratio * originalHeight);
+
+
+        Bitmap Thumb = new Bitmap(newWidth, newHeight, PixelFormat.Format24bppRgb);
+        Thumb.SetResolution(OriginalImg.HorizontalResolution, OriginalImg.VerticalResolution);
+
+        // Hvis billedet indeholder nogen form for transperans 
+        //(mere eller mindre gennemsigtig, eller en gennemsigtig baggrund) bliver det gjort her
+        Thumb.MakeTransparent();
+
+
+        // opret det nye billede
+        Graphics ThumbMaker = Graphics.FromImage(Thumb);
+        ThumbMaker.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+        ThumbMaker.DrawImage(OriginalImg,
+            new Rectangle(0, 0, newWidth, newHeight),
+            new Rectangle(0, 0, originalWidth, originalHeight),
+            GraphicsUnit.Pixel);
+
+        // encoding
+        ImageCodecInfo encoder;
+        string fileExt = System.IO.Path.GetExtension(Filename);
+        switch (fileExt)
+        {
+            case ".png":
+                encoder = GetEncoderInfo("image/png");
+                break;
+
+            case ".gif":
+                encoder = GetEncoderInfo("image/gif");
+                break;
+
+            default:
+                // default til JPG 
+                encoder = GetEncoderInfo("image/jpeg");
+                break;
+        }
+
+        EncoderParameters encoderParameters = new EncoderParameters(1);
+        encoderParameters.Param[0] = new EncoderParameter(Encoder.Quality, 100L);
+
+        // gem thumbnail i mappen /Images/Uploads/Thumbs/
+        Thumb.Save(Server.MapPath("~") + thumbuploadfolder + Filename, encoder, encoderParameters);
+
+        // Fjern originalbilledet, thumbnail mm, fra computerhukommelsen
+        OriginalImg.Dispose();
+        ThumbMaker.Dispose();
+        Thumb.Dispose();
+
+    }
+
+    #region encoding metode
+
+    private static ImageCodecInfo GetEncoderInfo(String mimeType)
+    {
+        ImageCodecInfo[] encoders = ImageCodecInfo.GetImageEncoders();
+        for (int i = 0; i < encoders.Length; i++)
+        {
+            if (encoders[i].MimeType == mimeType)
+            {
+                return encoders[i];
+            }
+        }
+        return null;
+    }
+    #endregion
+
+    #endregion
+}
