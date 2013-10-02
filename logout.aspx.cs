9f836ea15935e62f0ea4d5e033b122017a2408ed
chmodder <hotmailgateway@gmail.com>
2013-10-02 08:43:12 +0200

    created logout page
    not yet linked to from other pages

---
+++ logout.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class logout : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Hvis bruger er logget ind
+        if (Session["bruger_id"] != null)
+        {
+            // log bruger ud
+            Session.Abandon();
+        }
+        // Send brugeren tilbage til forsiden
+        Response.Redirect("default.aspx");
+
+    }
+}
